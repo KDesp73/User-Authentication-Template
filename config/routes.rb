45a3908ed35f4f698520b5f1a0a32b93afd07081
KDESP-LAPTOP\Konstantinos <kdesp2003@gmail.com>
2023-01-21 01:18:11 +0200

    Change password added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # GET /about and go to AboutController class 
   # in index function which points to dir: "app/views/about/index.html.erb" 
   # and this is the page we'll see
-  root to: "main#index", as: :root # or instead of root to:-> get "/", to:
+  root to: "main#index", as: :root # or instead of root to: -> get "/", to:
 
   get "/about", to: "about#index", as: :about # the routes name
 
@@ -13,4 +13,7 @@ Rails.application.routes.draw do
   post "/sign_in", to: "sessions#create"
   
   delete "/logout", to: "sessions#destroy"
+
+  get "/password", to: "passwords#edit", as: :edit_password
+  patch "/password", to: "passwords#updates" #patch request is for editing and stuff
 end
